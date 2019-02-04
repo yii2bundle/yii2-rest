54c8cf472e3e0559e9582a193671b128ac9e5101
Vitaliy <theyamshikov@yandex.ru>
2019-02-04 21:48:18 +0600

    fix

--- src/domain/repositories/base/BaseRestRepository.php
+++ src/domain/repositories/base/BaseRestRepository.php
@@ -2,8 +2,8 @@
 
 namespace yii2bundle\rest\domain\repositories\base;
 
+use yii2lab\rest\domain\traits\RestTrait;
 use yii2rails\domain\repositories\BaseRepository;
-use yii2tool\restclient\domain\traits\RestTrait;
 
 abstract class BaseRestRepository extends BaseRepository {
 
